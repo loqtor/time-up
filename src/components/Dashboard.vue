733f0f5f25a22ba227106e956b2ff781fd82128e
Martin Martinez <martin@alphero.com>
2017-04-16 09:00:05 +1200

    Shifted the timers to a mock file.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -8,44 +8,13 @@
 </template>
 
 <script>
+import timers from '@/mocks/timers'
+
 export default {
   name: 'dashboard',
   data () {
     return {
-      timers: [{
-        id: 1,
-        name: 'Timer 1',
-        rounds: [{
-          name: 'Round 1',
-          duration: 120,
-          type: 'workout'
-        }, {
-          name: 'Round 2',
-          duration: 30,
-          type: 'break'
-        }, {
-          name: 'Round 3',
-          duration: 120,
-          type: 'workout'
-        }]
-      },
-      {
-        id: 2,
-        name: 'Timer 2',
-        rounds: [{
-          name: 'Round 1',
-          duration: 120,
-          type: 'workout'
-        }, {
-          name: 'Round 2',
-          duration: 30,
-          type: 'break'
-        }, {
-          name: 'Round 3',
-          duration: 150,
-          type: 'workout'
-        }]
-      }]
+      timers: timers
     }
   }
 }
---
+++ src/mocks/timers.js
@@ -0,0 +1,34 @@
+export default [{
+  id: 1,
+  name: 'Timer 1',
+  rounds: [{
+    name: 'Round 1',
+    duration: 120,
+    type: 'workout'
+  }, {
+    name: 'Round 2',
+    duration: 30,
+    type: 'break'
+  }, {
+    name: 'Round 3',
+    duration: 120,
+    type: 'workout'
+  }]
+},
+{
+  id: 2,
+  name: 'Timer 2',
+  rounds: [{
+    name: 'Round 1',
+    duration: 120,
+    type: 'workout'
+  }, {
+    name: 'Round 2',
+    duration: 30,
+    type: 'break'
+  }, {
+    name: 'Round 3',
+    duration: 150,
+    type: 'workout'
+  }]
+}]
